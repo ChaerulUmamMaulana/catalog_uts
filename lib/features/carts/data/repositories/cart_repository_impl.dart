0cc21f1dbb0f6d6c4df2ba1ebf428444bac9bdd3
chaerulumammaulana <1123150132@global.ac.id>
2026-04-23 00:18:06 -0700

    cart
    update 2 cart_page.dart

--- lib/features/carts/data/repositories/cart_repository_impl.dart
+++ lib/features/carts/data/repositories/cart_repository_impl.dart
@@ -1,13 +1,15 @@
 
+import 'package:mycatalog/features/auth/data/models/product_model.dart';
+import 'package:mycatalog/features/carts/domain/repositories/cart_repository.dart';
+
 class CartRepositoryImpl implements CartRepository {
-  final List<Product> _items = [];
+  final List<ProductModel> _items = [];
   @override
-  List<Product> getCartItems() => List.unmodifiable(_items);
+  List<ProductModel> getCartItems() => List.unmodifiable(_items);
   @override
-  void addItem(Product product) => _items.add(product);
+  void addItem(ProductModel product) => _items.add(product);
   @override
-  @override
-  bool isItemInCart(String productId) => _items.any((p) => p.id == productId);
+  bool isItemInCart(int productId) => _items.any((p) => p.id == productId);
   
   @override
   void removeAllItems() {
